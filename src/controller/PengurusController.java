92f34d8d5e213d0991fd687c89111db03ed94165
M Hasan <accan83@gmail.com>
2016-06-10 14:46:30 +0700

    doLogin Function

--- src/controller/PengurusController.java
+++ src/controller/PengurusController.java
@@ -1,23 +1,15 @@
 package controller;
 
-import helper.ConnectionHelper;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-
-import model.Pengurus;
+import model.PengurusModel;
+import bean.Pengurus;
 
 public class PengurusController {
-	public static void doLogin(String nim, String pin) {
-//		ArrayList<Pengurus> p = Pengurus.find("nim", "=", "1341010008");
-//		for (Pengurus ps : p) {
-		Pengurus pengurus = Pengurus.findOne("nim", "=", nim);
-		if (pengurus != null) {
-			System.out.println(pengurus.getNama());
+	public static Pengurus doLogin(String nim, String pin) {
+		if (PengurusModel.verifyPassword(nim, pin)) {
+			Pengurus pengurus = PengurusModel.findOne("nim", "=", nim);
+			return pengurus;
 		}
-//		}
+		
+		return null;
 	}
 }
