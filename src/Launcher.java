e7afed5b36cee7115dc77920846ce847580f5621
M Hasan <accan83@gmail.com>
2016-06-12 15:10:09 +0700

    re-fine flow

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,50 +1,46 @@
+import helper.AppHelper;
+
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import view.LoginForm;
+import view.TransactionForm;
 
 public class Launcher {
-	public static String Log = "";
-	public static boolean adm = false;
-	
-	private static Object lock = new Object();
-	
+		
 	public static void main(String [] x){
-		LoginForm auth = new LoginForm();
-		auth.setVisible(true);
+		LoginForm.getInstance().setVisible(true);
 
-		Thread t = new Thread() {
-	        public void run() {
-	            synchronized(lock) {
-	                while (auth.isVisible()) {
-	                    try {
-	                        lock.wait();
-	                    } catch (InterruptedException e) {
-	                        e.printStackTrace();
-	                    }
-	                }
-	                System.out.println("Working now");
-	            }
-	        }
-	    };
-	    t.start();
-
-	    auth.addWindowListener(new WindowAdapter() {
+	    LoginForm.getInstance().addWindowListener(new WindowAdapter() {
 
 	        @Override
 	        public void windowClosing(WindowEvent arg0) {
-                auth.dispose();
+	        	LoginForm.getInstance().dispose();
 	        }
 
 	        @Override
 	        public void windowClosed(WindowEvent e) {
 	        	// TODO Auto-generated method stub
-	            synchronized (lock) {
-	                auth.dispose();
-	                lock.notify();
-	            }
+                if (AppHelper.getSesion() != null) {
+    	        	LoginForm.getInstance().reset();
+                	TransactionForm.getInstance().setVisible(true);
+                }
 	        }
 	    });
 
+	    TransactionForm.getInstance().addWindowListener(new WindowAdapter() {
+
+	        @Override
+	        public void windowClosing(WindowEvent arg0) {
+	        	TransactionForm.getInstance().dispose();
+	        }
+
+	        @Override
+	        public void windowClosed(WindowEvent e) {
+	        	// TODO Auto-generated method stub
+	        	AppHelper.setSesion(null);
+            	LoginForm.getInstance().setVisible(true);
+	        }
+	    });
 	}
 }
